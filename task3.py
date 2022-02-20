2bfc939527ea85f3e50ca239543e7b37e8a2afb7
E1ko0o <slava121231@mail.ru>
2022-02-20 15:39:12 +0300

    All prompts added + some fixes

--- task3.py
+++ task3.py
@@ -49,25 +49,33 @@ def bucket_sort(array):
     return result
 
 
+# Процедура для преобразования в двоичную кучу поддерева с корневым узлом i, что является индексом в arr[].
+# n - размер кучи
 def heapify(arr, n, i):
     largest = i
     left = 2 * i + 1
     right = 2 * i + 2
+    # Проверяем существует ли левый дочерний элемент больший, чем корень
     if left < n and arr[i] < arr[left]:
         largest = left
+    # Проверяем существует ли правый дочерний элемент больший, чем корень
     if right < n and arr[largest] < arr[right]:
         largest = right
+    # Заменяем корень, если нужно
     if largest != i:
         arr[i], arr[largest] = arr[largest], arr[i]
+        # Применяем heapify к корню.
         heapify(arr, n, largest)
 
 
 def heap_sort(arr):
     n = len(arr)
+    # Построение max-heap.
     for i in range(n // 2 - 1, -1, -1):
         heapify(arr, n, i)
+    # Один за другим извлекаем элементы
     for i in range(n - 1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]  # swap
+        arr[i], arr[0] = arr[0], arr[i]
         heapify(arr, i, 0)
 
 
