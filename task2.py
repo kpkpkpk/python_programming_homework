0981903e13bbcee1a0765248232bd76f852fe5ee
E1ko0o <slava121231@mail.ru>
2022-02-22 17:30:28 +0300

    Fixed second task

--- task2.py
+++ task2.py
@@ -35,8 +35,8 @@ def is_prime(number):
     try:
         number = int(number)
         if number > 1:
-            for i in range(2, number):
-                if (number % i) == 0:
+            for el in range(2, number):
+                if (number % el) == 0:
                     break
             else:
                 primes.append(number)
