d3ee4e7f413cf55b5e04564e412e24209566ec27
kpkpkpk <kostyalol15@gmail.com>
2022-03-24 19:53:22 +0300

    добавил все

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import math
+
 total_money = 10_000_000
 money_for_one = 3_333_333
 n = 21
 number_of_fields = 5  # номер периода, цена средняя, цена последняя (для закупки и продажи), изменение, капиталзация
 number_of_periods = 8  # количество периодов
-data = []
+data = []  # основное хранилище
 period_priceavg_pricelast_dif_cap = [[i] * number_of_fields for i in range(n * number_of_periods)]
 number_of_stocks_and_last_price = []
 
@@ -45,16 +47,6 @@ def read_csv_and_fill_data():
                     data.append(pair)
 
 
-# def reverse():
-#     import csv
-#
-#     with open("buffer.csv", encoding='utf8') as fr, open("YNDX.csv", "w", encoding='utf8') as fw:
-#         cr = csv.reader(fr, delimiter=";")
-#         cw = csv.writer(fw, delimiter=";")
-#         cw.writerow(next(cr))  # write title as-is
-#         cw.writerows(reversed(list(cr)))
-
-
 def count_evgen_part():
     for i_period in range(number_of_periods):
         sum_cap = 0
@@ -75,6 +67,74 @@ def count_evgen_part():
             number_of_stocks_and_last_price.append((k, round(i, 3)))
             print(f'{number_of_stocks_and_last_price[i_comp]} - numbers of'
                   f' {list(data[i_comp].keys())[0]} stock and price')
+
+
+# парная корреляция. Нахождение коэфф-а
+def find_correlation_coefficient(list_1, list_2):
+    average_one = sum(list_1) / len(list_1)
+    average_two = sum(list_2) / len(list_2)
+    first_k = 0
+    second_k = 0
+    third_k = 0
+
+    for i in range(len(list_1)):
+        first_k += math.pow(list_1[i] - average_one, 2)
+        second_k += math.pow(list_2[i] - average_two, 2)
+        third_k += (list_2[i] - average_two) * (list_1[i] - average_one)
+
+    return third_k / (math.sqrt(first_k * second_k))
+
+
+def anatoliy_task(list_of_company, s):
+    pair = []
+
+    for i in range(len(list_of_company) // 2, len(list_of_company)):
+        for j in range(0, len(list_of_company) // 2):
+            if ((list_of_company[j][-1][0] < list_of_company[j][-1][-1])
+                    and (list_of_company[i][-1][0] < list_of_company[i][-1][-1])
+                    and i != j):
+
+                k = math.fabs(find_correlation_coefficient(list_of_company[i][-1], list_of_company[j][-1]))
+                if len(pair) < 3:
+                    pair.append([k, i, j])
+                else:
+                    max = 0
+                    for index in range(len(pair)):
+                        if pair[index][0] > pair[max][0]: max = index
+                    pair[max] = [k, i, j]
+
+    packs = []
+    s /= 6
+    for i in pair:
+        packs.append([list_of_company[i[1]][0], (s) // list_of_company[i[1]][-1][-1]])
+        packs.append([list_of_company[i[2]][0], (s) // list_of_company[i[2]][-1][-1]])
+    return packs
+
+
+# Андрей
+def andrei_task(list_of_company, s):
+    pair = []
+
+    for i in range(len(list_of_company) // 2, len(list_of_company)):
+        for j in range(0, len(list_of_company) // 2):
+            if ((list_of_company[j][-1][0] < list_of_company[j][-1][-1])
+                    and (list_of_company[i][-1][0] < list_of_company[i][-1][-1])
+                    and i != j):
+
+                k = math.fabs(find_correlation_coefficient(list_of_company[i][-1], list_of_company[j][-1]))
+                if len(pair) < 3:
+                    pair.append([k, i, j])
+                else:
+                    min = 0
+                    for index in range(len(pair)):
+                        if pair[index][0] < pair[min][0]: min = index
+                    pair[min] = [k, i, j]
+
+    packs = []
+    for i in pair:
+        packs.append([list_of_company[i[1]][0], (s / 6) // list_of_company[i[1]][-1][-1]])
+        packs.append([list_of_company[i[2]][0], (s / 6) // list_of_company[i[2]][-1][-1]])
+    return packs
 
 
 if __name__ == '__main__':
