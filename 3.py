6347a16ce2dfcb1a43647c0384cf7921226a77fb
Vyacheslav Rashevskii <58101985+E1ko0o@users.noreply.github.com>
2022-05-31 10:58:10 +0300

    Lab 6

---
+++ 3.py
@@ -0,0 +1,50 @@
+import os
+from tkinter import *
+import re
+
+
+files = []
+file = ''
+
+
+def finish():
+    global file, var, regex
+    d = os.path.dirname(os.path.abspath(var.get()))
+    os.system(f'start chrome {d}\\{var.get()}')
+
+
+def create_rbs():
+    global var, row, column, regex
+    for i in range(len(files)):
+        f = files[i]
+        text = re.sub(regex, '', f.replace('.pdf', ''), 0)
+        rb = Radiobutton(master=frame, text=text, value=files[i], variable=var)
+        rb.select()
+        rb.grid(row=row, column=column)
+        row += 1
+
+
+regex = r"\d{4}-\d{2}-\d{2}-"
+for x in os.listdir():
+    if x.endswith(".pdf"):
+        files.append(x)
+
+row = 0
+column = 0
+
+window = Tk()
+window.title('Last lab ;)')
+frame = Frame(master=window)
+label = Label(master=frame, text='Which PDF file you want to open?')
+label.grid(row=row, column=0)
+row += 1
+
+var = StringVar()
+create_rbs()
+
+btn = Button(master=frame, text='Open', command=finish)
+btn.grid(row=row, column=0)
+
+window.minsize(250, 100)
+frame.pack()
+window.mainloop()
--- main.py
+++ main.py
@@ -1,145 +1,66 @@
-import math
-
-total_money = 10_000_000
-money_for_one = 3_333_333
-n = 21
-number_of_fields = 5  # номер периода, цена средняя, цена последняя (для закупки и продажи), изменение, капиталзация
-number_of_periods = 8  # количество периодов
-data = []  # основное хранилище
-period_priceavg_pricelast_dif_cap = [[i] * number_of_fields for i in range(n * number_of_periods)]
-number_of_stocks_and_last_price = []
+from tkinter import *
+from tkinter import messagebox
+import gspread
+import datetime
 
 
-def read_csv_and_fill_data():
-    import os
-    import re
-    root_dir = os.getcwd()
-    regex = re.compile('.*csv$')
-
-    for root, dirs, files in os.walk(root_dir):
-        index = 0
-        for file in files:
-            if regex.match(file):
-                with open(file, 'r', encoding='utf8') as f:
-                    local_data = []
-                    k = 1
-                    for line in f:
-                        if line.__contains__('Дата'):
-                            continue
-                        _, price, dif, cap = list(line.split(';'))
-                        period_priceavg_pricelast_dif_cap[index][1] += float(price.replace(',', '.'))
-                        period_priceavg_pricelast_dif_cap[index][3] += float(dif.replace(',', '.'))
-                        period_priceavg_pricelast_dif_cap[index][4] += float(cap.replace('\\n', '').replace(',', '.'))
-                        if k % 6 == 0:
-                            period_priceavg_pricelast_dif_cap[index][0] = index % 8
-                            period_priceavg_pricelast_dif_cap[index][1] = round(
-                                period_priceavg_pricelast_dif_cap[index][1] / k, 3)
-                            period_priceavg_pricelast_dif_cap[index][2] = round(float(price.replace(',', '.')), 3)
-                            period_priceavg_pricelast_dif_cap[index][3] = round(
-                                period_priceavg_pricelast_dif_cap[index][3] / k, 3)
-                            period_priceavg_pricelast_dif_cap[index][4] = round(
-                                period_priceavg_pricelast_dif_cap[index][4] / k, 3)
-                            k = 0
-                            local_data.append(period_priceavg_pricelast_dif_cap[index])
-                            index += 1
-                        k += 1
-                    pair = {str(file).replace('.csv', ''): local_data}
-                    data.append(pair)
+def alert(title, message, type_of_alert='info'):
+    show_method = getattr(messagebox, 'show{}'.format(type_of_alert))
+    show_method(title, message)
 
 
-def count_evgen_part():
-    for i_period in range(number_of_periods):
-        sum_cap = 0
-        list_caps_comp = []
-        list_prices = []
-        for i_comp in range(n):
-            data_for_period = list(data[i_comp].values())[0][i_period]
-            list_prices.append(data_for_period[2])
-            list_caps_comp.append(data_for_period[4])
-            sum_cap += data_for_period[4]
-        sum_cap = round(sum_cap, 3)
-        print(f'\n{sum_cap} - summary capitalization in {i_period} period')
-        list_caps_comp_percent = []
-        for i_comp in range(n):
-            list_caps_comp_percent.append(round(list_caps_comp[i_comp] / sum_cap, 3))
-            k = int((money_for_one * list_caps_comp_percent[i_comp]) // list_prices[i_comp])
-            i = round(list_prices[i_comp] * k, 3)
-            number_of_stocks_and_last_price.append((k, round(i, 3)))
-            print(f'{number_of_stocks_and_last_price[i_comp]} - numbers of'
-                  f' {list(data[i_comp].keys())[0]} stock and price')
+def finish(event):
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    global e_time, e_speed, e_name
+    flag = False
+    try:
+        time_in_minutes = str(int(e_time.get()))
+        speed = str(int(e_speed.get()))
+
+        cell_c1 = gsheet.acell('B1')
+        if str(cell_c1.value) != str(datetime.date.today()):
+            gsheet.update('B1', str(datetime.date.today()))
+        gsheet.update('B3', time_in_minutes)
+        gsheet.update('B4', speed)
+        alert('Success!', 'Data successfully updated!')
+        flag = True
+    except ValueError:
+        alert('Error!', 'You should input 2 integers', 'error')
+    if flag:
+        name = str(datetime.date.today()) + '-' + e_name.get()
+        fig, ax = plt.subplots()
+        fig.patch.set_visible(False)
+        ax.axis('off')
+        ax.axis('tight')
+        df = pd.DataFrame(gsheet.get_all_records())
+        ax.table(cellText=df.values, colLabels=df.columns, loc='center')
+        fig.tight_layout()
+        plt.savefig(f'{name}.pdf')
+        alert('Success!', 'Data successfully saved!')
 
 
-# парная корреляция. Нахождение коэфф-а
-def find_correlation_coefficient(list_1, list_2):
-    average_one = sum(list_1) / len(list_1)
-    average_two = sum(list_2) / len(list_2)
-    first_k = 0
-    second_k = 0
-    third_k = 0
+gc = gspread.oauth()
+gsheet = gc.open("MySheet").get_worksheet(0)
 
-    for i in range(len(list_1)):
-        first_k += math.pow(list_1[i] - average_one, 2)
-        second_k += math.pow(list_2[i] - average_two, 2)
-        third_k += (list_2[i] - average_two) * (list_1[i] - average_one)
+window = Tk()
+window.title('Last lab ;)')
+frame = Frame(master=window)
+label_time = Label(master=frame, text='Time in minutes:')
+label_time.grid(row=0, column=0)
+e_time = Entry(master=frame)
+e_time.grid(row=0, column=1)
+label_speed = Label(master=frame, text='Speed in meters per minutes:')
+label_speed.grid(row=1, column=0)
+e_speed = Entry(master=frame)
+e_speed.grid(row=1, column=1)
+label_name = Label(master=frame, text='Name of PDF file:')
+label_name.grid(row=2, column=0)
+e_name = Entry(master=frame)
+e_name.grid(row=2, column=1)
+btn_confirm = Button(master=frame, text='Confirm')
+btn_confirm.grid(row=3, column=0)
+btn_confirm.bind('<Button-1>', finish)
 
-    return third_k / (math.sqrt(first_k * second_k))
-
-
-def anatoliy_task(list_of_company, s):
-    pair = []
-
-    for i in range(len(list_of_company) // 2, len(list_of_company)):
-        for j in range(0, len(list_of_company) // 2):
-            if ((list_of_company[j][-1][0] < list_of_company[j][-1][-1])
-                    and (list_of_company[i][-1][0] < list_of_company[i][-1][-1])
-                    and i != j):
-
-                k = math.fabs(find_correlation_coefficient(list_of_company[i][-1], list_of_company[j][-1]))
-                if len(pair) < 3:
-                    pair.append([k, i, j])
-                else:
-                    max = 0
-                    for index in range(len(pair)):
-                        if pair[index][0] > pair[max][0]: max = index
-                    pair[max] = [k, i, j]
-
-    packs = []
-    s /= 6
-    for i in pair:
-        packs.append([list_of_company[i[1]][0], (s) // list_of_company[i[1]][-1][-1]])
-        packs.append([list_of_company[i[2]][0], (s) // list_of_company[i[2]][-1][-1]])
-    return packs
-
-
-# Андрей
-def andrei_task(list_of_company, s):
-    pair = []
-
-    for i in range(len(list_of_company) // 2, len(list_of_company)):
-        for j in range(0, len(list_of_company) // 2):
-            if ((list_of_company[j][-1][0] < list_of_company[j][-1][-1])
-                    and (list_of_company[i][-1][0] < list_of_company[i][-1][-1])
-                    and i != j):
-
-                k = math.fabs(find_correlation_coefficient(list_of_company[i][-1], list_of_company[j][-1]))
-                if len(pair) < 3:
-                    pair.append([k, i, j])
-                else:
-                    min = 0
-                    for index in range(len(pair)):
-                        if pair[index][0] < pair[min][0]: min = index
-                    pair[min] = [k, i, j]
-
-    packs = []
-    for i in pair:
-        packs.append([list_of_company[i[1]][0], (s / 6) // list_of_company[i[1]][-1][-1]])
-        packs.append([list_of_company[i[2]][0], (s / 6) // list_of_company[i[2]][-1][-1]])
-    return packs
-
-
-if __name__ == '__main__':
-    # reverse()
-
-    read_csv_and_fill_data()
-    print(data)
-    count_evgen_part()
+frame.pack()
+window.mainloop()
